e6a3d00fb3b382aab9197161d79d13d3775d9338
MargaritaMontes <l18140342@queretaro.tecnm.mx>
2022-10-20 23:54:25 -0500

    Tabla cambio

--- U3/tabla.js
+++ U3/tabla.js
@@ -1,23 +1,42 @@
-let tareas = [
-    {materia:"fisica", detalles:"Hacer lab fisica", fecha:new Date(2020, 4, 25)},
-    {materia:"mate", detalles:"Hacer lab fisica", fecha:new Date(2020, 4, 25)},
-]
+// Creating and adding data to first row of the table
+let row_1 = document.createElement('tr');
+let heading_1 = document.createElement('th');
+heading_1.innerHTML = "Sr. No.";
+let heading_2 = document.createElement('th');
+heading_2.innerHTML = "Name";
+let heading_3 = document.createElement('th');
+heading_3.innerHTML = "Company";
 
-let crearTabla = function(lista){
-    let stringTabla = <tr><th>Año</th><th>Monto</th></tr>
-    for(let tarea of lista){
-        let fila = "<tr> <td>"
-        fila += tarea.anio;
-        fila += "</td>"
+row_1.appendChild(heading_1);
+row_1.appendChild(heading_2);
+row_1.appendChild(heading_3);
+thead.appendChild(row_1);
 
-        fila += "<td>"
-        fila += tarea.monto;
-        fila += "</td>"
 
-        fila += "</tr>"
-        stringTabla += fila;
-    }
-    return stringTabla;
-};
+// Creating and adding data to second row of the table
+let row_2 = document.createElement('tr');
+let row_2_data_1 = document.createElement('td');
+row_2_data_1.innerHTML = "1.";
+let row_2_data_2 = document.createElement('td');
+row_2_data_2.innerHTML = "James Clerk";
+let row_2_data_3 = document.createElement('td');
+row_2_data_3.innerHTML = "Netflix";
 
-document.getElementById("tabla").innerHTML = crearTabla(tareas)
+row_2.appendChild(row_2_data_1);
+row_2.appendChild(row_2_data_2);
+row_2.appendChild(row_2_data_3);
+tbody.appendChild(row_2);
+
+// Creating and adding data to third row of the table
+let row_3 = document.createElement('tr');
+let row_3_data_1 = document.createElement('td');
+row_3_data_1.innerHTML = "2.";
+let row_3_data_2 = document.createElement('td');
+row_3_data_2.innerHTML = "Adam White";
+let row_3_data_3 = document.createElement('td');
+row_3_data_3.innerHTML = "Microsoft";
+
+row_3.appendChild(row_3_data_1);
+row_3.appendChild(row_3_data_2);
+row_3.appendChild(row_3_data_3);
+tbody.appendChild(row_3);
