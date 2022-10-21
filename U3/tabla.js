f579d2e6d5cfe5146c8999badeeb10fc4eb96d2f
MargaritaMontes <l18140342@queretaro.tecnm.mx>
2022-10-20 23:48:40 -0500

    Tabla dato

--- U3/tabla.js
+++ U3/tabla.js
@@ -1,6 +1,25 @@
-function calculo(){
-    var operador1 = document.forms[0].operador1.value
-    var operador2 = document.forms[0].operador2.value
-    var resultado = eval(operador1+operador2)
-    document.forms[0].resultado.value = result
-}
+mostrar("lista");
+
+let tareas = [
+    {materia:"fisica", detalles:"Hacer lab fisica", fecha:new Date(2020, 4, 25)},
+    {materia:"mate", detalles:"Hacer lab fisica", fecha:new Date(2020, 4, 25)},
+]
+
+let crearTabla = function(lista){
+    let stringTabla = <tr><th>Año</th><th>Monto</th></tr>
+    for(let tarea of lista){
+        let fila = "<tr> <td>"
+        fila += tarea.anio;
+        fila += "</td>"
+
+        fila += "<td>"
+        fila += tarea.monto;
+        fila += "</td>"
+
+        fila += "</tr>"
+        stringTabla += fila;
+    }
+    return stringTabla;
+};
+
+document.getElementById("tabla").innerHTML = crearTabla(tareas)
